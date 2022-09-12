dacc7930087e7b56550a77d3f427f14f3aa7e1c1
kamilsmr <kamils6035@gmail.com>
2022-09-12 14:23:23 +0300

    start

---
+++ 12092022.py
@@ -0,0 +1,13 @@
+# variables
+
+x=1  # int
+y=2,5 # float
+name='brad'#string
+is_cool=True #bool
+
+# multiple assignment
+
+x, y, name,is_cool =(1, 2.5, 'Brad',True)
+
+print(x, y, name,is_cool)
+
