94c97d0fd90624716c799be40fef052535de47b0
kamilsmr <kamils6035@gmail.com>
2022-09-17 11:33:29 +0300

    even

---
+++ 17092022.py
@@ -0,0 +1,13 @@
+# 1 ile 20 arasındaki çift rakamları yazdırma
+
+# count = 0
+# for number in range(1,20):
+#     if number % 2 == 0:
+#         count += 1
+#         print(number)
+
+# print(f"we have {count} even number")
+
+# 1 ile 20 arasındaki rakamları yazdırma
+# for number in range(1,20):
+#     print(number)
